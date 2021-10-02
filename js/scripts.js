d346e7751c8b948af5d9c920935e18fe15c05683
Irina <90955159+iriinaburiak@users.noreply.github.com>
2021-10-02 12:18:24 +0300

    lesson13

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,54 @@
-$(document).ready(function (){
 
+$(document).ready(function () {
+    $('.mobile-menu').on('click', function () {
+        $('.menu').slideToggle().css('right', '0px !important').css('left', 'auto');
+    })
+
+    const swiper = new Swiper('.gallery', {
+loop: true,
+navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+},
+spaceBetween: 30,
+
+    speed: 400,
+    breakpoints: {
+        320: {
+            slidesPerView: 1
+        },
+        560: {
+            slidesPerView: 2
+        },
+        768: {
+            slidesPerView: 3
+        },
+        900: {
+            slidesPerView: 4
+        }
+    }
 });
+//Tabs script
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 500);
+    });
+    //Anchor links
+
+$(function() {
+    var tab = $('#tabs .tabs-items > div');
+    tab.hide().filter(':first').show();
+
+    $('#tabs .tabs-nav a').click(function(){
+        tab.hide();
+        tab.filter(this.hash).show();
+        $('#tabs .tabs-nav a').removeClass('active');
+        $(this).addClass('active');
+        return false;
+    }).filter(':first').click();
+});
+
+})
