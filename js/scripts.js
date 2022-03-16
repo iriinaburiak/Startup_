9972921243859924c553512f5547d417b9a1c610
Irina <90955159+iriinaburiak@users.noreply.github.com>
2022-03-16 19:10:15 +0300

    done

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,51 @@
-
 $(document).ready(function () {
     $('.mobile-menu').on('click', function () {
         $('.menu').slideToggle().css('right', '0px !important').css('left', 'auto');
     })
 
     const swiper = new Swiper('.gallery', {
-loop: true,
-navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-},
-spaceBetween: 30,
-
-    speed: 400,
-    breakpoints: {
-        320: {
-            slidesPerView: 1
+        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
         },
-        560: {
-            slidesPerView: 2
-        },
-        768: {
-            slidesPerView: 3
-        },
-        900: {
-            slidesPerView: 4
+        spaceBetween: 30,
+        speed: 400,
+        breakpoints: {
+            320: {
+                slidesPerView: 1
+            },
+            560: {
+                slidesPerView: 2
+            },
+            768: {
+                slidesPerView: 3
+            },
+            900: {
+                slidesPerView: 4
+            }
         }
-    }
-});
+    });
+    // Tabs script
+    $(function() {
+        var tab = $('#tabs .tabs-items > div');
+        tab.hide().filter(':first').show();
 
-$(function() {
-    var tab = $('#tabs .tabs-items > div');
-    tab.hide().filter(':first').show();
+        $('#tabs .tabs-nav a').click(function(){
+            tab.hide();
+            tab.filter(this.hash).show();
+            $('#tabs .tabs-nav a').removeClass('active');
+            $(this).addClass('active');
+            return false;
+        }).filter(':first').click();
+    });
+    // Anchor links
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
 
-    $('#tabs .tabs-nav a').click(function(){
-        tab.hide();
-        tab.filter(this.hash).show();
-        $('#tabs .tabs-nav a').removeClass('active');
-        $(this).addClass('active');
-        return false;
-    }).filter(':first').click();
-});
-
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 500);
+    });
+    
 })
